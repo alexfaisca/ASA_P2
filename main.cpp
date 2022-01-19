8b9c93bd9386754af2b1f4df34782abdcd756c82
Lameiras <87664900+PLameiras@users.noreply.github.com>
2022-01-19 19:50:24 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+using namespace std;
+
+struct Node{
+    int key;
+    struct Node *left, *right;
+};
+
+Node* newNode(int key){
+    Node *aux = new Node;
+    aux->key = key;
+    aux->left = aux->right = NULL;
+    return aux;
+}
+
+Node* genealogicalTree(){
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string line;
+    while(cin) {
+        getline(cin, line);
+
+    }
     return 0;
 }
