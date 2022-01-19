f073e5bdb8a5364dd614c2e272eb98d6216181c1
Lameiras <87664900+PLameiras@users.noreply.github.com>
2022-01-19 20:26:21 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ struct Node{
     struct Node *left, *right;
 };
 
+struct geneologicalTree{
+    Node *nodes;
+};
+
 Node* newNode(int key){
     Node *aux = new Node;
     aux->key = key;
@@ -13,18 +17,33 @@ Node* newNode(int key){
     return aux;
 }
 
-Node* genealogicalTree(){
+Node *findNode(geneologicalTree *tree, int key){
+    Node *source[];
+}
+
+Node *createNode(geneologicalTree *tree, int key){
 
 }
 
+geneologicalTree *addChild(Node *parent, Node *child){
+    if(parent->left == NULL) {
+        parent->left = child;
+    } else{
+        parent->right = child;
+    }
+}
+
 int main() {
-    int v1, v2, vertices, edges, target1, target2;
+    int target1, target2, vertices, edges, v1, v2;
     scanf("%d %d\n", &target1, &target2);
     scanf("%d %d\n", &vertices, &edges);
 
     while(vertices-- > 0) {
         scanf("%d %d\n", &v1, &v2);
+        if(findNode(v1) == NULL)
+            struct Node* node = newNode(v1);
+        addChild(findNode(v1), createNode(v2));
     }
-    return 0;
+
     return 0;
 }
