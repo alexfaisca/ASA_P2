70512c4502fe6ff46276a4a59b1b04d52d0e4a61
alexandre <alex.c.2002@gmail.com>
2022-01-20 15:57:50 +0000

    blueprint

--- main.cpp
+++ main.cpp
@@ -1,49 +1,32 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-struct Node{
-    int key;
-    struct Node *left, *right;
-};
 
-struct geneologicalTree{
-    Node *nodes;
-};
+void mark_parent_nodes(vector< vector<int> > graph_transpose, vector<int> node_markings, int mark, int max_mark);
 
-Node* newNode(int key){
-    Node *aux = new Node;
-    aux->key = key;
-    aux->left = aux->right = NULL;
-    return aux;
-}
-
-Node *findNode(geneologicalTree *tree, int key){
-    Node *source[];
-}
-
-Node *createNode(geneologicalTree *tree, int key){
-
-}
-
-geneologicalTree *addChild(Node *parent, Node *child){
-    if(parent->left == NULL) {
-        parent->left = child;
-    } else{
-        parent->right = child;
-    }
-}
 
 int main() {
     int target1, target2, vertices, edges, v1, v2;
     scanf("%d %d\n", &target1, &target2);
     scanf("%d %d\n", &vertices, &edges);
 
+    vector< vector<int> > graph_transpose(vertices);
+    vector<int> node_markings(vertices,0);
+    bool valid_graph = true;
+
     while(vertices-- > 0) {
         scanf("%d %d\n", &v1, &v2);
-        if(findNode(v1) == NULL)
-            struct Node* node = newNode(v1);
-        addChild(findNode(v1), createNode(v2));
+        graph_transpose[v2-1].push_back(v1);
+        if(graph_transpose[v2-1].size() > 2) {
+            valid_graph = false;
+            break;
+        }
     }
-
+    if(!valid_graph) {
+        cout << "0" << endl;
+        return 0;
+    }
     return 0;
 }
