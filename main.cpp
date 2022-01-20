8e220d49688318d4503c2e033e862829cf9975ea
Lameiras <87664900+PLameiras@users.noreply.github.com>
2022-01-20 22:30:34 +0000

    ...

--- main.cpp
+++ main.cpp
@@ -3,13 +3,17 @@
 
 using namespace std;
 
-void mark_parent_nodes(vector< vector<int> > *graph_transpose, vector<int> *node_markings, int node, int mark, int max_mark){
+int mark_parent_nodes(vector< vector<int> > *graph_transpose, vector<int> *node_markings, int node, int mark, int max_mark, int depth){
+    if (depth == 0)
+        return -1;
     for(int parent : (*graph_transpose)[node-1]){
         if((*node_markings)[parent-1] == mark) {
             (*node_markings)[parent - 1] = max_mark;
         }
-        mark_parent_nodes(graph_transpose, node_markings, parent, mark, max_mark);
+        if(mark_parent_nodes(graph_transpose, node_markings, parent, mark, max_mark, depth-1) == -1)
+            return -1;
     }
+    return 0;
 }
 
 int main() {
@@ -20,29 +24,29 @@ int main() {
     vector< vector<int> > graph_transpose(vertices);
     vector<int> node_markings(vertices,0);
     bool output = false, valid_graph = true;
+    int i;
 
-    for(int i = edges; i != 0 && scanf("%d %d", &v1, &v2) != EOF; i--) {
+    for(i = 0; i != edges && scanf("%d %d", &v1, &v2) != EOF; i++) {
         graph_transpose[v2-1].push_back(v1);
         if(graph_transpose[v2-1].size() > 2) {
             valid_graph = false;
             break;
         }
     }
-        if(!valid_graph) {
-        cout << "0 ";
+
+    if(!valid_graph || i != edges || mark_parent_nodes(&graph_transpose, &node_markings, target1, 0, 1, edges) == -1 ||
+            mark_parent_nodes(&graph_transpose, &node_markings, target2, 1, 2, edges) == -1) {
+        cout << "0" << endl;
         return 0;
     }
 
-    mark_parent_nodes(&graph_transpose, &node_markings, target1, 0, 1);
-    mark_parent_nodes(&graph_transpose, &node_markings, target2, 1, 2);
-
-    for(int i = 0; i < static_cast<int>(node_markings.size()); i++) {
+    for(i = 0; i < static_cast<int>(node_markings.size()); i++) {
         if(node_markings[i] == 2) {
-            mark_parent_nodes(&graph_transpose, &node_markings, i + 1, 2, 1);
+            mark_parent_nodes(&graph_transpose, &node_markings, i + 1, 2, 1, edges);
         }
     }
 
-    for(int i = 0; i < static_cast<int>(node_markings.size()); i++) {
+    for(i = 0; i < static_cast<int>(node_markings.size()); i++) {
         if(node_markings[i] == 2) {
             cout << i+1 << ' ';
             output = true;
